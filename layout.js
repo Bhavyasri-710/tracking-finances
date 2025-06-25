e76f151c819a1d26d08f08a8bdd7487680c74a2e
Bhavyasri-710 <bellanabhavyasri@gmail.com>
2025-06-25 09:33:23 +0530

    Add files via upload

--- layout.js
+++ layout.js
@@ -1,29 +1,5 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/header";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Aiconomy",
-  description: "Expence Traker",
+const AuthLayout = ({ children }) => {
+  return <div className="flex justify-center pt-40">{children}</div>;
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <head>
-          <link rel="icon" href="/logo-sm.png" sizes="any"/>
-        </head>
-        <body className={`${inter.className}`}>
-          <Header />
-          <main className="min-h-screen">{children}</main>
-          <Toaster richColors />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
+export default AuthLayout;
