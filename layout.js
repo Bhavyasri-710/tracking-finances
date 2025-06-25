d50ee1b53bd5b4d35b3d4adf0cbdae856ab98b57
Bhavyasri-710 <bellanabhavyasri@gmail.com>
2025-06-25 09:34:53 +0530

    Add files via upload

--- layout.js
+++ layout.js
@@ -1,5 +1,7 @@
-const AuthLayout = ({ children }) => {
-  return <div className="flex justify-center pt-40">{children}</div>;
+import React from "react";
+
+const MainLayout = ({ children }) => {
+  return <div className="container mx-auto my-32">{children}</div>;
 };
 
-export default AuthLayout;
+export default MainLayout;
